f5abb6889f2b03ef76146784e3476d7e3836dc4f
Deny Prasetyo <deny.prasetyo@go-pay.co.id>
2018-04-16 07:18:56 +0530

    Add rule to check whether engineer already doing shift a in current day or not

---
+++ src/main/java/id/jasoet/wof/scheduler/domain/rule/SingleShiftDaily.java
@@ -0,0 +1,33 @@
+package id.jasoet.wof.scheduler.domain.rule;
+
+
+import id.jasoet.wof.scheduler.domain.entity.DailyShift;
+import id.jasoet.wof.scheduler.domain.entity.Engineer;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * This rule checks whether an engineer already take support shift at current day.
+ * This class leverage Spring {@link Component} annotation, will be handled by Spring Context
+ */
+@Component
+public class SingleShiftDaily implements SchedulePredicate {
+    /**
+     * Get current day shift and check whether engineer already take shift or not.
+     *
+     * @param dailyShifts       Evaluated DailyShifts
+     * @param currentShiftIndex Evaluated index of DailyShifts
+     * @param engineer          Evaluated Engineer
+     * @return true if engineer already take support shift at current day, false if otherwise
+     */
+    @Override
+    public boolean evaluate(List<DailyShift> dailyShifts, int currentShiftIndex, Engineer engineer) {
+        if (currentShiftIndex < 0 || currentShiftIndex >= dailyShifts.size()) {
+            return false;
+        }
+
+        final DailyShift currentShift = dailyShifts.get(currentShiftIndex);
+        return currentShift.getFirstHalf() == engineer || currentShift.getFirstHalf() == engineer;
+    }
+}
