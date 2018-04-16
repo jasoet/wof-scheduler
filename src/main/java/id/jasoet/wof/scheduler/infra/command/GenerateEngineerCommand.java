342bf0dbca346c87f93f723a193255e10d2bd878
Deny Prasetyo <deny.prasetyo@go-pay.co.id>
2018-04-16 22:12:51 +0530

    add command to Generate Engineer and Save it

---
+++ src/main/java/id/jasoet/wof/scheduler/infra/command/GenerateEngineerCommand.java
@@ -0,0 +1,10 @@
+package id.jasoet.wof.scheduler.infra.command;
+
+import id.jasoet.wof.scheduler.domain.entity.Engineer;
+import id.jasoet.wof.scheduler.infra.command.base.Command;
+import reactor.core.publisher.Flux;
+
+public interface GenerateEngineerCommand extends Command<Engineer, Integer> {
+    @Override
+    Flux<Engineer> execute(Integer request);
+}
---
+++ src/main/java/id/jasoet/wof/scheduler/infra/command/impl/GenerateEngineerCommandImpl.java
@@ -0,0 +1,25 @@
+package id.jasoet.wof.scheduler.infra.command.impl;
+
+import id.jasoet.wof.scheduler.domain.entity.Engineer;
+import id.jasoet.wof.scheduler.infra.command.GenerateEngineerCommand;
+import id.jasoet.wof.scheduler.infra.command.base.AbstractCommand;
+import id.jasoet.wof.scheduler.infra.service.EngineerService;
+import lombok.experimental.var;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+@Component
+public class GenerateEngineerCommandImpl extends AbstractCommand<Engineer, Integer> implements GenerateEngineerCommand {
+    private EngineerService engineerService;
+
+    public GenerateEngineerCommandImpl(EngineerService engineerService) {
+        this.engineerService = engineerService;
+    }
+
+    @Override
+    public Flux<Engineer> execute(Integer request) {
+        var engineers = engineerService.generateEngineers();
+        engineerService.saveAll(engineers);
+        return Flux.fromIterable(engineers);
+    }
+}
